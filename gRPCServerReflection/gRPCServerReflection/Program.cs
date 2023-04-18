7b94856c71d0c969f3d696e76dca1587e697885c
Robson Jean de Souza <robson.jsouza@outlook.com>
2023-04-18 17:12:49 -0300

    improves service url handling

--- gRPCServerReflection/gRPCServerReflection/Program.cs
+++ gRPCServerReflection/gRPCServerReflection/Program.cs
@@ -3,11 +3,10 @@ using gRPCServerReflection.Services;
 using Microsoft.Extensions.Logging.Console;
 using ProtoBuf.Grpc.Server;
 
+var url = args.Length >= 1 ? args[1] : "https://localhost:7131";
+
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
-    // this is to force the local address and port.
-    // Useful for working in a team where different ports from machine to machine would cause settings to be changed accordingly.
-    // Example: Args = new string[] { "https://localhost:7131" }
     Args = args
 });
 
@@ -31,4 +30,6 @@ app.MapGrpcService<GreeterService>();
 app.MapCodeFirstGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
+// this url parameter is to force the local address and port.
+// Useful for working in a team where different ports from machine to machine would cause settings to be changed accordingly.
+app.Run(url);
